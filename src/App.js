564d68a6dae76e9530f93c70567e2785ff7c6125
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-08-28 20:19:32 -0700

    transfer state into Game component

--- src/App.js
+++ src/App.js
@@ -8,31 +8,24 @@ function Square(props) {
   
       <button className='ttt_parameter' onClick={props.onClick}>
         {props.value}
-        
       </button>
     )
 }
 
 class Board extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      current: Array(9).fill(null)
-    }
-  }
-
-  handleClick = (i) => {
-
-    this.setState({
-
-    })
-  }
+    // constructor (props) {
+  //   super(props)
+  //   this.state = {
+  //     current: Array(9).fill(null),
+  //     xsTurn: true
+  //   }
+  // }
 
   renderSquare = (i) => {
     return (
       <Square 
-      value={this.state.current[i]}
-      onClick={() => {this.handleClick(i)}}/>
+      value={this.props.currentGame[i]}
+      onClick={() => {this.props.onClick(i)}}/>
     )
   }
   render () {
@@ -59,11 +52,30 @@ class Board extends Component {
 }
 
 class Game extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentGame: Array(9).fill(null),
+      xsTurn: true
+    }
+  }
+
+    handleClick = (i) => {
+    if (this.state.currentGame[i] !== null) {
+      return
+    }
+    const newSquares = this.state.currentGame.slice()
+    newSquares[i] = this.state.xsTurn ? 'X' : 'O'
+    this.setState({
+      currentGame: newSquares,
+      xsTurn: !this.state.xsTurn
+    })
+  }
   render () {
     return (
       <div className='ttt_game'>
         <h1 className='game-title'>TTT Game</h1>
-        <Board />
+        <Board onClick={this.handleClick} currentGame={this.state.currentGame}/>
       </div>
     )
   }
