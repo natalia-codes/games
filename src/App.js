b9a7236d16abc2acbb706de807d3cd16b2622d92
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-08-28 20:14:39 -0700

    squares moved into board

--- src/App.js
+++ src/App.js
@@ -1,43 +1,73 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class Square extends Component {
-  render () {
+
+
+function Square(props) {
     return (
-      <div className='ttt_parameter'>X</div>
+  
+      <button className='ttt_parameter' onClick={props.onClick}>
+        {props.value}
+        
+      </button>
     )
-  }
 }
 
 class Board extends Component {
-  render() {
-    return (
-      <div className='ttt_board'>
-        <Square key = '0' />
-        <Square key = '1' />
-        <Square key = '2' />
-        <Square key = '3' />
-        <Square key = '4' />
-        <Square key = '5' />
-        <Square key = '6' />
-        <Square key = '7' />
-        <Square key = '8' />
-        </div>
+  constructor (props) {
+    super(props)
+    this.state = {
+      current: Array(9).fill(null)
+    }
+  }
 
+  handleClick = (i) => {
+
+    this.setState({
+
+    })
+  }
+
+  renderSquare = (i) => {
+    return (
+      <Square 
+      value={this.state.current[i]}
+      onClick={() => {this.handleClick(i)}}/>
+    )
+  }
+  render () {
+    return (
+      <div className='game-board'>
+        <div className='game-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='game-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='game-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
     )
   }
 }
 
 class Game extends Component {
-  render() {
+  render () {
     return (
-      <div className='game'>
-        <h1 className='game_title'>TTT Game</h1>
+      <div className='ttt_game'>
+        <h1 className='game-title'>TTT Game</h1>
         <Board />
       </div>
     )
   }
 }
 
-export default Game
 
+export default Game
